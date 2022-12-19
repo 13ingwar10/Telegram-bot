99788fc0a9e042d04abea7ebc417d9d96d0c4679
Igor <g-igor92@mail.ru>
2022-12-19 23:01:10 +0500

    Проект чат-бота готов

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import telebot
+import telebot, wikipedia, re
 from telebot import types
 import Create_notebook
 import csv
@@ -8,18 +8,22 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.reply_to(message, 'Привет! Я бот-всезнайка. Чтобы узнать что-то новенькое, сначала зарегистрируйся')
     Create_notebook.create_list()
-
-@bot.message_handler(commands=['button'])
-def button_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Зарегистрироваться")
-    markup.add(item1)
-    bot.send_message(message.chat.id, 'Приступим к регистрации:', reply_markup=markup)
-    bot.register_next_step_handler(message, register_me)
+    item1 = types.KeyboardButton("Вступить в клуб печенек")
+    item2 = types.KeyboardButton("Найти информацию")
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id, 'Привет! Я бот-всезнайка. Ты можешь узнать что-то новенькое и вступить в клуб печенек!', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
+
+def root(message):
+    if message.text == "Вступить в клуб печенек":
+        bot.register_next_step_handler(message, register_me)
+    elif message.text == "Найти информацию":
+        msg = bot.send_message(message.chat.id, 'Введите слово для поиска информации')
+        bot.register_next_step_handler(msg, handle_text)
+
 def register_me(message):
     msg = bot.send_message(message.chat.id, 'Введите Имя')
     bot.register_next_step_handler(msg, enter_fio)
@@ -38,6 +42,32 @@ def enter_surname(message, new_user):
 def enter_tel(message, new_user):
     new_user.append(message.text)
     bot.send_message(message.chat.id, 'Регистрация завершена!')
-    print(new_user)
+
+    with open('notebook.csv', 'a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f, delimiter=';')
+        writer.writerow(new_user)
+
+wikipedia.set_lang("ru")
+
+def get_wiki(request):
+    try:
+        page = str(wikipedia.page(request))
+        page = page.content[:1000]
+        page = list(page.split('.'))
+        page =  page[:-1]
+
+        wiki_output = ''
+        for string in page:
+            if not ('==' in string):
+                wiki_output = wiki_output + string + '.'
+        re.sub('\([^()]*\)', '', wiki_output)
+        return wiki_output
+    except Exception as e:
+        return 'Ничего не найдено'
+
+def handle_text(message):
+    bot.send_message(message.chat.id, get_wiki(message.text))
+
+
 
 bot.infinity_polling()
